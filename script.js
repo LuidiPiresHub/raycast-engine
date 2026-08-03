496dee35ac2f6711eee52a21a61ae7be6c61f25f
LuidiPiresHub <luidihot@gmail.com>
2026-08-03 00:40:36 -0300

    feat: melhoria no fullscreen e menu para inciar o game

--- script.js
+++ script.js
@@ -9,8 +9,12 @@ import { drawMinimap } from './renderer/drawMinimap.js'
 import { drawPlayer } from './renderer/drawPlayer.js'
 
 const mainTag = document.querySelector('.main')
+const menu = document.querySelector('.menu')
 
+let animationId = 0;
 let lastTime = 0
+let started = false
+
 const loop = (currentTime) => {
   const deltaTime = Math.min((currentTime - lastTime) / 1000, 0.1)
   lastTime = currentTime
@@ -19,35 +23,57 @@ const loop = (currentTime) => {
   movePlayer(deltaTime)
   updateCamera(deltaTime)
   draw3DMap()
-  requestAnimationFrame(loop)
+  animationId = requestAnimationFrame(loop)
 }
 
-const main = () => {
-  drawMinimap()
-  requestAnimationFrame(loop)
-
-  mainTag.addEventListener('click', async () => {
-    await mainTag.requestFullscreen()
-    await mainTag.requestPointerLock()
-  });
+const setEvents = () => {
+  started = true
 
   const { map3DSizeY } = world
 
+  document.addEventListener('keydown', updateKeysState)
+  document.addEventListener('keyup', updateKeysState)
+
   document.addEventListener('mousemove', ({ movementX, movementY }) => {
     if (document.pointerLockElement === mainTag) {
-      const sensitivityX = 0.002;
-      const sensitivityY = 0.8;
+      const sensitivityX = 0.002
+      const sensitivityY = 0.8
+      const nextPitch = camera.pitch - movementY * sensitivityY
 
       player.angle += movementX * sensitivityX
-
-      const nextPitch = camera.pitch - movementY * sensitivityY;
-
       camera.pitch = Math.max(-map3DSizeY, Math.min(map3DSizeY, nextPitch));
     }
   });
 
-  document.addEventListener('keydown', updateKeysState)
-  document.addEventListener('keyup', updateKeysState)
+  document.addEventListener('fullscreenchange', () => {
+    if (document.fullscreenElement === mainTag) {
+      menu.classList.add('hidden')
+      lastTime = performance.now()
+
+      if (!animationId) {
+        animationId = requestAnimationFrame(loop)
+      }
+
+    } else {
+      menu.classList.remove('hidden')
+      cancelAnimationFrame(animationId)
+      animationId = 0
+    }
+  })
+}
+
+const startGame = async () => {
+  if (!started) setEvents()
+  await mainTag.requestFullscreen()
+  await mainTag.requestPointerLock()
+}
+
+const main = () => {
+  drawMinimap()
+  drawPlayer()
+  draw3DMap()
+
+  mainTag.addEventListener('click', startGame);
 }
 
 main()
